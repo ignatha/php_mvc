db1489176dc46efc26ece94425f43885b7e2f3c6
Prabowo Ignatha <prabowoid.2@gmail.com>
2024-12-04 14:25:15 +0700

    Update part 1

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class HomeController {
+
+    public static function index()
+    {
+        echo "Ini dicetak dari method index HomeController";
+    }
+}
+
---
+++ app/app.php
@@ -0,0 +1,6 @@
+<?php
+namespace App;
+
+use App\Controllers\HomeController;
+
+HomeController::index();
